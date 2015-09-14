112fedff3c41c00c64a7c7d5a10b6e7172e880d3
dobbybabee <dobbybabee@gmail.com>
2015-09-14 19:50:32 -0400

    Updated schema.sql, and with it, had to update prog
    New way of storing things in database, so I had to update how info was
    obtained in flask/used in templates.

--- riot_api.py
+++ riot_api.py
@@ -9,24 +9,19 @@ config = ConfigParser.RawConfigParser()
 config.read('configuration')
 
 SECRET_KEY = config.get("Riot API", "SECRET_KEY")
-
-# Returns match details of a given match id in Json format.
-# Includes Timeline by default.
-## TODO make sure match_id is valid. If not, return null.
-def get_match(match_id):
-    response = None
+    
+def get_version():
     try:
-        response = urllib2.urlopen("https://na.api.pvp.net/api/lol/na/v2.2/match/" + str(match_id) + "?includeTimeline=true&api_key=" + SECRET_KEY)
+        response = urllib2.urlopen("https://ddragon.leagueoflegends.com/realms/na.json")
+        return json.loads(response.read())
     except urllib2.HTTPError, e:
         print "ERROR! " + str(e.code)
     except urllib2.URLError, e:
         print "Error! " + str(e.reason())
     except httplib.HTTPException, e:
         print "HTTPException error occurred"
-    if response is not None:
-        return json.loads(response.read())
     return None
-    
+
 def get_event_frames(match_json):
     if match_json is not None:
         # This just obtains the frame specific data we want. We can ignore the rest.
@@ -35,7 +30,8 @@ def get_event_frames(match_json):
         except:
             return None
     return None
-   
+
+# Gets recent matches from Riot's API   
 def get_recent_matches():
     epoch = datetime.datetime.utcfromtimestamp(0)
     now = datetime.datetime.now()
@@ -55,64 +51,14 @@ def get_recent_matches():
         print "HTTPException error occurred"
     return None
     
-def get_current_champions():
-    response = None
-    try:
-        response = urllib2.urlopen("http://ddragon.leagueoflegends.com/cdn/5.16.1/data/en_US/champion.json")
-        return json.loads(response.read())
-    except urllib2.HTTPError, e:
-        print "ERROR! " + str(e.code)
-    except urllib2.URLError, e:
-        print "Error! " + str(e.reason())
-    except httplib.HTTPException, e:
-        print "HTTPException error occurred"
-    return None
-        
-def get_champ_stats(summoner_id, champ_id):
-    response = None
-    try:
-        response = urllib2.urlopen("https://na.api.pvp.net/api/lol/na/v1.3/stats/by-summoner/" + str(summoner_id) + "/ranked?season=SEASON2015&api_key=" + SECRET_KEY)
-        champions = json.loads(response.read())["champions"]
-        for champ in champions:
-            if champ["id"] == champ_id:
-                print champ["stats"]
-                break
-        return champ["stats"]
-    except urllib2.HTTPError, e:
-        print "ERROR! " + str(e.code)
-    except urllib2.URLError, e:
-        print "Error! " + str(e.reason())
-    except httplib.HTTPException, e:
-        print "HTTPException error occurred"
-    return None
-    
+def get_summoners():
+    return get_from_cdn('/data/en_US/summoner.json')
+ 
 def get_items():
-    response = None
-    try:
-        response = urllib2.urlopen("http://ddragon.leagueoflegends.com/cdn/5.16.1/data/en_US/item.json")
-        items = json.loads(response.read())["data"]
-        return items
-    except urllib2.HTTPError, e:
-        print "ERROR! " + str(e.code)
-    except urllib2.URLError, e:
-        print "Error! " + str(e.reason())
-    except httplib.HTTPException, e:
-        print "HTTPException error occurred"
-    return None
+    return get_from_cdn('/data/en_US/item.json')
 
 def get_champions():
-    response = None
-    try:
-        response = urllib2.urlopen("http://ddragon.leagueoflegends.com/cdn/5.16.1/data/en_US/champion.json")
-        items = json.loads(response.read())["data"]
-        return items
-    except urllib2.HTTPError, e:
-        print "ERROR! " + str(e.code)
-    except urllib2.URLError, e:
-        print "Error! " + str(e.reason())
-    except httplib.HTTPException, e:
-        print "HTTPException error occurred"
-    return None
+    return get_from_cdn('/data/en_US/champion.json')
     
 # Sorry RITO
 def get_acs_info(url):
@@ -129,7 +75,10 @@ def get_acs_info(url):
         else:
             print str(e.code)
     except urllib2.URLError, e:
-        print "Error! " + str(e.reason())
+        print e
+        print "Oops we pissed Riot off, wait 10"
+        time.sleep(10)
+        return get_acs_info(url)
     except httplib.HTTPException, e:
         print "HTTPException error occurred"
     return None
@@ -148,3 +97,18 @@ def get_timeline_url(url):
     index = url.find('?gameHash')
     new_url = url[:index] + '/timeline' + url[index:]
     return new_url
+
+def get_from_cdn(end_url):
+    response = None
+    version = get_version()
+    try:
+        response = urllib2.urlopen(version['cdn'] + '/' + version['dd'] + end_url)
+        items = json.loads(response.read())["data"]
+        return items
+    except urllib2.HTTPError, e:
+        print "ERROR! " + str(e.code)
+    except urllib2.URLError, e:
+        print "Error! " + str(e.reason())
+    except httplib.HTTPException, e:
+        print "HTTPException error occurred"
+    return None
